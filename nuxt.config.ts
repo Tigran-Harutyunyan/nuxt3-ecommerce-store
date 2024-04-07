075e60270e105e4a5a5e538a39d3bfc10388e07f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-07 23:35:51 +0400

    added configs  and package JSON

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/google-fonts', '@pinia/nuxt', '@nuxt/image', 'nuxt-headlessui'],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  build: {
+    transpile: ['vue-clerk', '@clerk/clerk-js', "@nuxt/image", '@pinia/nuxt'],
+  },
+  runtimeConfig: {
+    public: {
+      clerkPublishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    },
+    clerkSecretKey: process.env.CLERK_SECRET_KEY,
+    storeId: process.env.STORE_ID,
+    apiUrl: process.env.API_URL
+
+  },
+  googleFonts: {
+    subsets: 'latin',
+    families: {
+      Poppins: {
+        wght: [600],
+      },
+    }
+  },
+  image: {
+
+  }
+})  
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const animate = require("tailwindcss-animate");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {},
+  plugins: [animate],
+};
