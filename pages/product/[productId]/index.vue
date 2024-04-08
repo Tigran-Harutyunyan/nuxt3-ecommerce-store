f2812705c624c9b8160b277f317b71936c319d8b
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-08 14:30:37 +0400

    added incomplete Product page

---
+++ pages/product/[productId]/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { Product } from "@/types";
+const route = useRoute();
+
+const { data: product } = useFetch<Product>(
+  `/api/product/${route.params.productId}`,
+  {
+    server: true,
+  }
+);
+</script>
+
+<template>
+  <div class="bg-white">
+    <Container>
+      <div class="px-4 py-10 sm:px-6 lg:px-8">
+        <div class="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+          <Gallery :images="product?.images || []" />
+          <div class="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+            <Info :data="product" />
+          </div>
+        </div>
+        <hr class="my-10" />
+        <!-- <ProductList title="Related Items" :items="suggestedProducts" /> -->
+      </div>
+    </Container>
+  </div>
+</template>
