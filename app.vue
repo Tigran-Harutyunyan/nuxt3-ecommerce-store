f8fc0941b1fbe23cc652827b30d959ea3b60698a
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-08 14:33:00 +0400

    app components

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Toaster } from "@/components/ui/sonner";
+</script>
+<template>
+  <NuxtLoadingIndicator color="#f43f5e" />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <Toaster />
+</template>
