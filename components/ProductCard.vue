bad440d95bd6dc3e2a22f9b1500cc01fcf7f2701
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-08 13:58:41 +0400

    created product list and product card components

---
+++ components/ProductCard.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import IconButton from "@/components/ui/IconButton.vue";
+import Currency from "@/components/ui/Currency.vue";
+
+import type { Product } from "@/types";
+import { Expand, ShoppingCart } from "lucide-vue-next";
+import { useCart } from "@/store/cart";
+import { useModals } from "@/store/modals";
+
+const cart = useCart();
+const { onOpen } = useModals();
+
+interface ProductCard {
+  data: Product;
+}
+
+const { data } = defineProps<ProductCard>();
+
+const router = useRouter();
+
+const handleClick = () => {
+  router.push(`/product/${data?.id}`);
+};
+</script>
+
+<template>
+  <div
+    @click="handleClick"
+    class="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+  >
+    <div class="aspect-square rounded-xl relative">
+      <NuxtImg
+        :src="data.images?.[0]?.url"
+        alt=""
+        fill
+        class="aspect-square object-cover rounded-md mx-auto"
+      />
+      <div
+        class="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5"
+      >
+        <div class="flex gap-x-6 justify-center">
+          <IconButton @click="onOpen(data)">
+            <Expand :size="20" class="text-gray-600" />
+          </IconButton>
+
+          <IconButton @click="cart.addItem(data)">
+            <ShoppingCart :size="20" class="text-gray-600" />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <p class="font-semibold text-lg">{{ data.name }}</p>
+      <p class="text-sm text-gray-500">{{ data.category?.name }}</p>
+    </div>
+
+    <div class="flex items-center justify-between">
+      <Currency :value="data?.price" />
+    </div>
+  </div>
+</template>
---
+++ components/ProductList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { type Product } from "@/types";
+
+interface ProductListProps {
+  title: string;
+  items: Product[] | undefined | null;
+}
+const props = defineProps<ProductListProps>();
+const { title, items } = toRefs(props);
+</script>
+
+<template>
+  <div class="space-y-4" v-if="items">
+    <h3 class="font-bold text-3xl">{{ title }}</h3>
+    <NoResults v-if="items.length === 0" />
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+    >
+      <ProductCard v-for="item in items" :key="item.id" :data="item" />
+    </div>
+  </div>
+</template>
