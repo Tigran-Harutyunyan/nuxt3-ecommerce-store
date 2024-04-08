c9cc71f809392571450eb0d788285a053458222f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-08 13:58:19 +0400

    created Navbar componet

---
+++ components/MainNav.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+import { type Category } from "@/types";
+
+interface MainNavProps {
+  data: Category[] | null;
+}
+
+const props = defineProps<MainNavProps>();
+const { data } = toRefs(props);
+
+const route = useRoute();
+
+const routes = computed(() => {
+  const pathname = route.fullPath;
+
+  if (!data.value) return [];
+
+  return data?.value.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
+});
+</script>
+
+<template>
+  <nav class="mx-6 flex items-center space-x-4 lg:space-x-6">
+    <NuxtLink
+      v-for="route in routes"
+      :key="route.href"
+      :to="route.href"
+      :class="
+        cn(
+          'text-sm font-medium transition-colors hover:text-black',
+          route.active ? 'text-black' : 'text-neutral-500'
+        )
+      "
+    >
+      {{ route.label }}
+    </NuxtLink>
+  </nav>
+</template>
---
+++ components/NavBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import MainNav from "@/components/MainNav.vue";
+import Container from "@/components/Container.vue";
+import NavbarActions from "@/components/NavbarActions.vue";
+import { type Category } from "@/types";
+
+const { data: categories } = useFetch<Category[]>("/api/categories", {
+  server: true,
+});
+</script>
+
+<template>
+  <div class="border-b">
+    <Container>
+      <div class="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+        <NuxtLink to="/" class="ml-4 flex lg:ml-0 gap-x-2">
+          <p class="font-bold text-xl">STORE</p>
+        </NuxtLink>
+        <MainNav :data="categories" />
+        <NavbarActions />
+      </div>
+    </Container>
+  </div>
+</template>
