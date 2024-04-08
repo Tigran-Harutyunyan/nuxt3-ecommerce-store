5681613a27d0f7ac9f282e6511a67ee60cfbec53
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-08 14:06:52 +0400

    Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="bg-white border-t flex-0">
+    <div class="mx-auto py-10">
+      <p class="text-center text-xs text-black">
+        &copy; 2024 Store, Inc. All rights reserved.
+      </p>
+    </div>
+  </footer>
+</template>
