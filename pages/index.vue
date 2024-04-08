008b13a6ea2b9724e74894ba9f628144de094b32
Tigran <harutyunyan.tigran1975@gmail.com>
2024-04-08 13:57:36 +0400

    created index page

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { Product, Billboard as BillboardType } from "@/types";
+
+definePageMeta({
+  middleware: ["auth"],
+});
+
+const { data: billboards } = useFetch<BillboardType[]>("/api/billboards", {
+  server: true,
+});
+
+const { data: products } = useFetch<Product[]>(
+  "/api/products?isFeatured=true",
+  {
+    server: true,
+  }
+);
+
+const billboard = computed(() => {
+  if (billboards.value && Array.isArray(billboards.value)) {
+    return billboards.value[0];
+  }
+  return {};
+});
+</script>
+
+<template>
+  <Container>
+    <div class="space-y-10 pb-10">
+      <Billboard :data="billboard" />
+      <div class="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" :items="products" />
+      </div>
+    </div>
+  </Container>
+</template>
